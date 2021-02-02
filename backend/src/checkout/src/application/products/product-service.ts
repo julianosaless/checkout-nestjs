eb41ac490e05f2584b3d4541fb12daf5e5edc426
Juliano Sales <juliano.salesg@gmail.com>
2021-02-02 20:27:22 -0300

    Backend: Create Product Services #2: add product application

---
+++ backend/src/checkout/src/application/products/product-service.ts
@@ -0,0 +1,26 @@
+import { BaseRepository } from "src/common/domain/repository/base.repository";
+import { ProductRepository } from 'src/infrastructure/repository/products/product-repository';
+import { Product } from "src/domain/products/product";
+import { ProductDto } from "./product-dto";
+
+export class ProductService {
+
+  private repository: BaseRepository<Product>;
+  constructor(private productRepository: ProductRepository) {
+    this.repository = productRepository;
+  }
+
+  public async get(id: string): Promise<ProductDto> {
+    const product = await this.repository.find(id);
+    return this.map(product);
+  }
+
+  public async getAll(): Promise<ProductDto[]> {
+    const products = await this.repository.findAll();
+    return products.map(product => this.map(product));
+  }
+
+  private map(product: Product): ProductDto {
+    return new ProductDto(product.id, product.name, product.price, product.promotionId);
+  }
+}
