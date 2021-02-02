546c45545ddd150a54c7a091226b4e4e7fb870c7
Juliano Sales <juliano.salesg@gmail.com>
2021-02-02 19:44:04 -0300

    Backend: Create Product Services #2: add missing columns

--- backend/src/checkout/src/domain/products/product.ts
+++ backend/src/checkout/src/domain/products/product.ts
@@ -1,7 +1,9 @@
 import { Entity, Column } from 'typeorm';
 
-import { IAggregateRoot } from 'src/common/domain/aggregate-root.interface';
 import { EntityBase } from 'src/common/domain/entity-base';
+import { IAggregateRoot } from 'src/common/domain/aggregate-root.interface';
+import { Promotion } from './promotion';
+import { PromotionType } from './promotion-type';
 
 @Entity('products')
 export class Product extends EntityBase implements IAggregateRoot {
@@ -9,11 +11,20 @@ export class Product extends EntityBase implements IAggregateRoot {
   readonly name: string;
   @Column()
   readonly price: number;
-
+  @Column()
+  readonly promotionId: string;
+  @Column()
+  promotion: Promotion;
 
   constructor(name: string, price: number) {
     super();
     this.name = name;
     this.price = price;
   }
+
+  public addPromotion(name: string, minQuantity: number, promotionType: PromotionType): Product {
+    this.promotion = new Promotion(name, minQuantity, promotionType);
+    return this;
+  }
+
 }
--- backend/src/checkout/src/domain/products/promotion.ts
+++ backend/src/checkout/src/domain/products/promotion.ts
@@ -1,9 +1,15 @@
+import { Column, Entity } from 'typeorm';
+
 import { EntityBase } from 'src/common/domain/entity-base';
 import { PromotionType } from './promotion-type';
 
+@Entity('promotion')
 export class Promotion extends EntityBase {
+  @Column()
   readonly name: string;
+  @Column()
   readonly minQuantity: number;
+  @Column()
   readonly promotionType: PromotionType;
 
   constructor(name: string, minQuantity: number, promotionType: PromotionType) {
