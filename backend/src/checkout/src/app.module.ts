2610889092502e25fb79c121eecee329a4a80a5e
Juliano Sales <juliano.salesg@gmail.com>
2021-02-02 19:41:07 -0300

    Infrastructure: add initial configuration of type orm

--- backend/src/checkout/src/app.module.ts
+++ backend/src/checkout/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
+import { DomainModule } from './domain/domain.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.TYPEORM_HOST,
+    port: parseInt(process.env.TYPEORM_PORT),
+    database: process.env.TYPEORM_DATABASE,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    synchronize: true,
+    logging: true,
+  }),
+    InfrastructureModule,
+    DomainModule]
 })
-export class AppModule {}
+export class AppModule { }
