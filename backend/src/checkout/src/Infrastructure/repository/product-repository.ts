48051e465c2411dc09acea9b6e1d2189c601af25
Juliano Sales <juliano.salesg@gmail.com>
2021-02-02 19:45:20 -0300

    clean up application

--- backend/src/checkout/src/Infrastructure/repository/product-repository.ts
+++
@@ -1,5 +0,0 @@
-import { BaseRepository } from "src/common/domain/repository/base.repository";
-import { Product } from "src/domain/products/product";
-
-export class ProductRepository extends BaseRepository<Product>{
-}
