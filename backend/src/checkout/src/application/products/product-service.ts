8ed6fafc70b78e5ce8a969aea84dbc893960ae1d
Juliano Sales <juliano.salesg@gmail.com>
2021-02-02 23:59:33 -0300

    Backend: Create Product Services #2: fix inject repository problem

--- backend/src/checkout/src/application/products/product-service.ts
+++ backend/src/checkout/src/application/products/product-service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 
-import { BaseRepository } from "src/common/domain/repository/base.repository";
 import { ProductRepository } from 'src/infrastructure/repository/products/product-repository';
 import { Product } from "src/domain/products/product";
 import { ProductDto } from "./product-dto";
 
+const ProductRepo = () => Inject('ProductRepo');
+
 @Injectable()
 export class ProductService {
 
-  private repository: BaseRepository<Product>;
-  constructor(private productRepository: ProductRepository) {
-    this.repository = productRepository;
-  }
+  constructor(
+    @ProductRepo() private readonly repository: ProductRepository,
+  ) { }
 
   public async get(id: string): Promise<ProductDto> {
     const product = await this.repository.find(id);
