87de7fbacc183d46f6d4cb55e106caaf162efb46
Juliano Sales <juliano.salesg@gmail.com>
2021-02-03 17:42:12 -0300

    Create Product service: fix route promotions

--- backend/src/checkout/src/api/Products/ProductController.ts
+++ backend/src/checkout/src/api/Products/ProductController.ts
@@ -1,5 +1,4 @@
 import { Controller, Get, HttpStatus, Put, Param, HttpCode } from "@nestjs/common";
-import { get } from "http";
 import { ProductDto } from "src/application/products/product-dto";
 
 import { ProductService } from "src/application/products/product-service";
@@ -10,6 +9,20 @@ export class ProductController {
 
   constructor(private service: ProductService) { }
 
+  @Get("promotions")
+  @HttpCode(200)
+  public async getAllPromotions(): Promise<PromotionDto[]> {
+    return await this.service
+      .getAllPromotions();
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  async getBy(@Param('id') id: string): Promise<ProductDto> {
+    return await this.service
+      .get(id);
+  }
+
   @Get()
   @HttpCode(200)
   public async getAll(): Promise<ProductDto[]> {
@@ -17,9 +30,4 @@ export class ProductController {
       .getAll();
   }
 
-  @Get("promotions")
-  @HttpCode(200)
-  public async getAllPromotions(): Promise<PromotionDto[]> {
-    return await this.service.getAllPromotions();
-  }
 }
