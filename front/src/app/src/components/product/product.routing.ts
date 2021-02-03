e9ee6b55a248339aff7d2ad212c5e2e5ec11033b
Juliano Sales <juliano.salesg@gmail.com>
2021-02-03 16:14:17 -0300

    Frontend: Create product catalog #4

---
+++ front/src/app/src/components/product/product.routing.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+
+import { IndexComponent } from '../Index/index.component';
+import { CartProductsComponent } from './cart-products/cart-product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+export const ProductRoutes: Routes = [
+  {
+    path: 'products',
+    children: [
+      {
+        path: '',
+        component: IndexComponent,
+      },
+      {
+        path: 'all',
+        component: ProductListComponent,
+      },
+      {
+        path: 'cart-items',
+        component: CartProductsComponent,
+      },
+      {
+        path: ':id',
+        component: ProductDetailComponent,
+      },
+    ],
+  },
+];
