5fe67ed42ec0cfc2fbd5a1146668dc9844404bad
Juliano Sales <juliano.salesg@gmail.com>
2021-02-04 17:02:53 -0300

    Infrastructure: Register Cart repository

--- backend/src/checkout/src/Infrastructure/infrastructure.module.ts
+++ backend/src/checkout/src/Infrastructure/infrastructure.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+
+import { CartRepositoryModule } from './repository/carts/cart-repository.module';
 import { ProductRepositoryModule } from './repository/products/product-repository.module';
 
 @Module({
-  imports: [ProductRepositoryModule],
-  exports: [ProductRepositoryModule],
+  imports: [ProductRepositoryModule, CartRepositoryModule],
+  exports: [ProductRepositoryModule, CartRepositoryModule],
 })
 export class InfrastructureModule { }
